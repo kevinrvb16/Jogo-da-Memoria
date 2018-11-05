927671b99deffedc111bccbffaccdbc505629e8c
Lígia <ligia.sell@hotmail.com>
2018-11-05 19:00:29 -0200

    main

---
+++ Jogo-da-Memoria/src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+            Jogo frameJogo= new Jogo();
+            frameJogo.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frameJogo.setSize(800, 500);
+            frameJogo.setVisible(true);
+            frameJogo.setResizable(true);
+            }
+}
+
