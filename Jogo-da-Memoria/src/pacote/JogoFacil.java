841550f5a751f8c78620d7c5ce8577d9243443fa
mudouasenha <mudouasenha@gmail.com>
2018-11-09 00:53:01 -0200

    Imagens redimensionadas, código tentando armazenar 2 JButtons no ActionEvent

--- Jogo-da-Memoria/src/pacote/JogoFacil.java
+++ Jogo-da-Memoria/src/pacote/JogoFacil.java
@@ -12,54 +12,79 @@ import java.math.*;
 
 public class JogoFacil extends JFrame { //implements Jogo
 	private JButton[] cartas;
+
+	
 	MouseEvent clique;
 	private final ImageIcon[] arrayFundos = {
 			new ImageIcon(getClass().getResource("FUNDO.jpg")),
 			new ImageIcon(getClass().getResource("1.jpg")),
-			new ImageIcon(getClass().getResource("2.jpg"))
+			new ImageIcon(getClass().getResource("2.jpg")),
+			new ImageIcon(getClass().getResource("3.jpg")),
+			new ImageIcon(getClass().getResource("4.jpg")),
+			new ImageIcon(getClass().getResource("5.jpg")),
+			new ImageIcon(getClass().getResource("6.jpg")),
+			//new ImageIcon(getClass().getResource("7.jpg"))
 	};
 	
 		public JogoFacil() {
 			super("Jogo da Memoria");
 			setLayout(new GridBagLayout());
-			cartas = new JButton[2];
+			cartas = new JButton[6];
 			
 			//colocando fundo
-			for(int i = 0; i < 2; i++) {
+			for(int i = 0; i < 6; i++) {
 			cartas[i] = new JButton(arrayFundos[0]);}
 			
 			//adicionando cartas ao frame?
-			for(int i = 0; i < 2; i++) {
+			for(int i = 0; i < 6; i++) {
 			add(cartas[i]);}
 			
 			
 			//adicionando handlers
 			ButtonHandler handler = new ButtonHandler();
-			for(int i = 0; i < 2; i++) {
+			for(int i = 0; i < 6; i++) {
 			cartas[i].addActionListener(handler);
 			}
 			
 		}
-			
-		
-		
 		
 		//clase listener com erro
 	class ButtonHandler implements ActionListener {
-
+		
 		@Override
 		public void actionPerformed(ActionEvent event) {
-			JButton botao1 = (JButton) event.getSource();
-			int random = (int) (Math.random() * 2);
-			arrayFundos[0] = new ImageIcon(getClass().getResource("1.jpg"));
-			arrayFundos[1] = new ImageIcon(getClass().getResource("2.jpg"));
-			botao1.setIcon(arrayFundos[random]);
-			System.out.println(botao1);
+			JButton source = null;
+			JButton botao1 = null;
+			JButton botao2 = null;
+				
+				while(botao1 == null && botao2 == null) {
+					source = (JButton) event.getSource();
+					if(botao1 == null)
+						botao1 = source;
+					//erro, volta botao2 nulo?
+					else
+						if(source == botao1)
+							return;
+						else
+							botao2 = source;
+				}
+				System.out.println(botao1);
+				System.out.println(botao2);
+			
+			}
+			
+			//int random = (int) (Math.random() * 6) + 1;
+			//arrayFundos[0] = new ImageIcon(getClass().getResource("1.jpg"));
+			//arrayFundos[1] = new ImageIcon(getClass().getResource("2.jpg"));
+			//botao1.setIcon(arrayFundos[random]);			
+			
+			
 			
 		}
 		
+	
 	}
-	}
+
 
 
 
