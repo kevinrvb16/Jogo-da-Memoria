e0c55518ef022c5566e6fa21ea8dd688b98fd62e
Kevin <kevin.velez@grad.ufsc.br>
2018-11-12 20:46:18 -0200

    criado controlador

---
+++ Jogo-da-Memoria/src/pacote/controladorJogo.java
@@ -0,0 +1,21 @@
+package pacote;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class controladorJogo extends JFrame {
+
+    protected JButton iniciar;
+    protected JButton ranking;
+    protected JButton opcaoNivel;
+    protected JButton pausar;
+    protected JButton sair;
+    
+    
+    public controladorJogo(){
+        
+        super("Jogo memoria");
+        
+        
+    } 
+}
