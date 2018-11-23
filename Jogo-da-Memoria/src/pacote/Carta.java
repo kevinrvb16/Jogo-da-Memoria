7468e42c328bb6c61a01c1e02725b0899a2a85b0
mudouasenha <mudouasenha@gmail.com>
2018-11-23 02:14:56 -0200

    Add ControllerJogo, IControllerJogo

--- Jogo-da-Memoria/src/pacote/Carta.java
+++ Jogo-da-Memoria/src/pacote/Carta.java
@@ -29,7 +29,11 @@ public class Carta extends JButton {
 		public Carta() {
 
 		}
-
+		
+		
+		
+		
+		
 		public int getNumeroImagem() {
 			return numeroImagem;
 		}
@@ -38,9 +42,29 @@ public class Carta extends JButton {
 			this.numeroImagem = numeroImagem;
 		}
 		
+		
+		public ImageIcon getImageIcon() {
+			return imageIcon;
+		}
+		
 		public void setImageIcon(ImageIcon imageIcon) {
 			this.imageIcon = imageIcon;
 		}
 		
+		public int getX() {
+			return x;
+		}
+		
+		public void setX(int x) {
+			this.x = x;
+		}
+		
+		public int getY() {
+			return y;
+		}
+		
+		public void setY(int y) {
+			this.y = y;
+		}
 		
 }
